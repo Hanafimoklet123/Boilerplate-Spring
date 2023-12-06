aef03531d3f02b6c7f733f616090d8d87bd2f612
Hasbi <hasbi@digi-id.id>
2023-12-06 10:59:05 +0700

    feat: create repo

---
+++ src/test/java/com/replica/hsk/ReplicaHskSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.replica.hsk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplicaHskSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
